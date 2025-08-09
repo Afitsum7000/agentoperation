4b023a54b99b848d3b5d5bd281bc004c7078822e
Afitsum7000 <fitsumkassaye217@gmail.com>
2025-08-09 19:12:59 +0300

    done working on getuser endpoin

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 console.log("this is a test file for ai agent purpose ");
 console.log("this is where developer write thier code like the actual code so when they submit with the commit message it will be tracked") 
 console.log("loyality program and getuser endpoint done")
+console.log("this is for test purpose")
 
