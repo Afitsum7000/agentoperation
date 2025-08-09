1d07ffbd3b4ab59bdb1955f5c2ba548d2b67b9f1
Afitsum7000 <fitsumkassaye217@gmail.com>
2025-08-09 10:01:00 +0300

    getuser endpoint done and loyality new feature is also done

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.log("this is a test file for ai agent purpose ");
 console.log("this is where developer write thier code like the actual code so when they submit with the commit message it will be tracked") 
+console.log("loyality program and getuser endpoint done")
 
