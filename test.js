59cb7b806c35f0ac9864a94851c1291f795b2c79
Afitsum7000 <fitsumkassaye217@gmail.com>
2025-08-09 09:23:35 +0300

    implement /uers endpoint and implement loyality program, refactore the code

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
 console.log("this is a test file for ai agent purpose ");
-console.log("this is a test file") // IGNORE
 console.log("this is where developer write thier code like the actual code so when they submit with the commit message it will be tracked") 
-console.log("add GET /users with pagination and input validation  ")
-console.log("this one is for test to track developer commit message")
+
